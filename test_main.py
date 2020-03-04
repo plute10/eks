ea8b836cee3ffa1432e701e7d8c74319695f5e9e
정진희 <jinheechung@sk.com>
2020-03-04 09:39:03 +0900

    indentation fixed

--- test_main.py
+++ test_main.py
@@ -15,26 +15,25 @@ PASSWORD = 'huff-puff'
 @pytest.fixture
 def client():
 	os.environ['JWT_SECRET'] = SECRET
-    main.APP.config['TESTING'] = True
-    client = main.APP.test_client()
+	main.APP.config['TESTING'] = True
+	client = main.APP.test_client()
 
-    yield client
+	yield client
 
 
 
 def test_health(client):
 	response = client.get('/')
-    assert response.status_code == 200
-    assert response.json == 'Healthy'
+	assert response.status_code == 200
+	assert response.json == 'Healthy'
 
 
 def test_auth(client):
-	body = {'email': EMAIL,
-            'password': PASSWORD}
-    response = client.post('/auth', 
+	body = {'email': EMAIL,'password': PASSWORD}
+	response = client.post('/auth', 
                            data=json.dumps(body),
                            content_type='application/json')
 
-    assert response.status_code == 200
-    token = response.json['token']
-    assert token is not None
+	assert response.status_code == 200
+	token = response.json['token']
+	assert token is not None
